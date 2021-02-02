f670a625a22f5a70aecf152e24f5e52bb47510e2
hamed <hamedmonji30@gmail.com>
2021-02-02 22:20:38 +0330

    add path item animation

--- lib/board.dart
+++ lib/board.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -60,6 +61,19 @@ class Path extends StatefulWidget {
 class _PathState extends State<Path> with SingleTickerProviderStateMixin {
   bool _addCourse = false;
   FocusNode focusNode = FocusNode();
+  GlobalKey<AnimatedListState> listKey = GlobalKey();
+
+  final List<String> items = [];
+  double itemSize = 100;
+  double initialSize;
+  TextStyle style = TextStyle(
+      fontSize: 22, color: Color(0xff4B4B4B), fontWeight: FontWeight.w300);
+  @override
+  void initState() {
+    super.initState();
+    initialSize = _textSize("Item name", style);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -78,41 +92,114 @@ class _PathState extends State<Path> with SingleTickerProviderStateMixin {
               filter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
               child: Container(
                 decoration: BoxDecoration(color: Colors.white.withOpacity(0.4)),
-                child: AnimatedSize(
-                  duration: Duration(milliseconds: 300),
-                  vsync: this,
-                  child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: !_addCourse
-                          ? Center(
-                              child: Container(
-                                height: 46,
-                                width: 46,
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: AppColors.colorPrimary),
-                                    borderRadius: BorderRadius.circular(46)),
-                              ),
-                            )
-                          : Row(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                TextField(
-                                  autofocus: true,
-                                  focusNode: focusNode,
-                                  decoration: InputDecoration(
-                                    hintText: "Course name",
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: AnimatedContainer(
+                            duration: Duration(milliseconds: 300),
+                            height: 46,
+                            constraints: BoxConstraints(
+                                maxHeight: 46,
+                                minHeight: 46,
+                                minWidth: 46,
+                                maxWidth: !_addCourse
+                                    ? 46
+                                    : max(itemSize, initialSize) + 80),
+                            child: _addCourse
+                                ? Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 4, horizontal: 30),
+                                    child: Center(
+                                      child: TextField(
+                                        focusNode: focusNode,
+                                        decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          hintText: "Item name",
+                                        ),
+                                        onChanged: (value) {
+                                          setState(() {
+                                            itemSize = _textSize(value, style);
+                                          });
+                                        },
+                                        onSubmitted: (value) {
+                                          items.add(value);
+                                          listKey.currentState
+                                              .insertItem(items.length - 1);
+                                          setState(() {
+                                            _addCourse = false;
+                                          });
+                                        },
+                                        style: style,
+                                      ),
+                                    ),
+                                  )
+                                : null,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: _addCourse
+                                        ? Colors.primaries[(items.length + 1) %
+                                            Colors.primaries.length]
+                                        : AppColors.colorPrimary),
+                                borderRadius: BorderRadius.circular(46)),
+                          ),
+                        ),
+                        AnimatedList(
+                          key: listKey,
+                          reverse: true,
+                          itemBuilder: (context, index, animation) {
+                            if (index == 0) {
+                              return SizeTransition(
+                                axis: Axis.horizontal,
+                                axisAlignment: 1,
+                                sizeFactor: animation,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Center(
+                                    child: Container(
+                                      height: 60,
+                                      width: 60,
+                                      decoration: BoxDecoration(
+                                          color: Colors.primaries[
+                                              index % Colors.primaries.length],
+                                          borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(24),
+                                              bottomRight:
+                                                  Radius.circular(24))),
+                                    ),
                                   ),
-                                  onSubmitted: (value) {},
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      color: Color(0xff4B4B4B),
-                                      fontWeight: FontWeight.w300),
-                                )
-                              ],
-                            )),
-                ),
+                                ),
+                              );
+                            }
+                            return SizeTransition(
+                              axis: Axis.horizontal,
+                              axisAlignment: 1,
+                              sizeFactor: animation,
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Center(
+                                  child: Container(
+                                    height: 46,
+                                    width: 46,
+                                    decoration: BoxDecoration(
+                                        color: Colors.primaries[
+                                            index % Colors.primaries.length],
+                                        borderRadius:
+                                            BorderRadius.circular(66)),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          initialItemCount: items.length,
+                        )
+                      ],
+                    )),
               ),
             ),
             decoration: BoxDecoration(
@@ -123,6 +210,15 @@ class _PathState extends State<Path> with SingleTickerProviderStateMixin {
         ],
       ),
     );
+  }
+
+  double _textSize(String text, TextStyle style) {
+    final TextPainter textPainter = TextPainter(
+        text: TextSpan(text: text, style: style),
+        maxLines: 1,
+        textDirection: TextDirection.ltr)
+      ..layout(minWidth: 0, maxWidth: double.infinity);
+    return textPainter.size.width;
   }
 }
 
