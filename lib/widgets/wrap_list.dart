9054e5ecc6240b3163c936508bfc3388330f88bb
hamed <hamedmonji30@gmail.com>
2021-02-11 18:21:09 +0330

    add horizontal and vertical wrap list

--- lib/widgets/wrap_list.dart
+++ lib/widgets/wrap_list.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class WrapList extends StatelessWidget {
+class HorizontalWrapList extends StatelessWidget {
   final List<Widget> children;
   final Widget leading;
-  const WrapList({Key key, this.leading, this.children}) : super(key: key);
+  const HorizontalWrapList({Key key, this.leading, @required this.children})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return IntrinsicHeight(
       child: Row(
         children: [
-          leading,
+          if (leading != null) leading,
           Expanded(
             child: Column(mainAxisSize: MainAxisSize.min, children: [
               SingleChildScrollView(
@@ -18,6 +19,35 @@ class WrapList extends StatelessWidget {
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: children,
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VerticalWrapList extends StatelessWidget {
+  final List<Widget> children;
+  final Widget leading;
+  const VerticalWrapList({Key key, this.leading, @required this.children})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: children,
                   ),
                 ),
